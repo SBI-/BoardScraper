fca9deccd2c18f300464f38d46a27548c34a47c9
SBI- <pascal.huerlimann@gmail.com>
2016-04-03 17:20:01 +0200

    Example of parsing the root forum with jaxb, should be canonnical for all other types.

--- src/main/java/ch/sbi/scraper/Main.java
+++ src/main/java/ch/sbi/scraper/Main.java
@@ -1,5 +1,16 @@
 package ch.sbi.scraper;
 
+import ch.sbi.scraper.DataTypes.Board;
+import ch.sbi.scraper.DataTypes.Boards;
+import ch.sbi.scraper.DataTypes.Categories;
+import ch.sbi.scraper.DataTypes.Category;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -13,11 +24,21 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            URL url = new URL(args[0] + "/xml/boards.php");
-            InputStream stream = url.openStream();
-            stream.close();
+            URL url = new URL(args[0] + "xml/boards.php");
+
+            String packageName = "ch.sbi.scraper.DataTypes";
+            JAXBContext context = JAXBContext.newInstance(packageName);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Source source = new StreamSource(url.openStream());
+            // using the unmarshal overload with a specific class is necessary if the class doesn't have / isn't a root
+            // element.
+            JAXBElement<Categories> forum = unmarshaller.unmarshal(source, Categories.class);
         } catch (IOException e) {
             logger.severe("Url failed to open. Reason: " + e);
+        } catch (JAXBException e) {
+            logger.info("JAXB Error: " + e);
+            e.printStackTrace();
         }
     }
 
