c63349c8447503a94db43d0c3080d7d4da5ac11c
SBI- <pascal.huerlimann@gmail.com>
2016-04-03 15:07:06 +0200

    Set up logging for the current client implementation.

--- src/main/java/ch/sbi/scraper/Main.java
+++ src/main/java/ch/sbi/scraper/Main.java
@@ -1,6 +1,29 @@
 package ch.sbi.scraper;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
+        try {
+            URL url = new URL(args[0] + "/xml/boards.php");
+            InputStream stream = url.openStream();
+            stream.close();
+        } catch (IOException e) {
+            logger.severe("Url failed to open. Reason: " + e);
+        }
+    }
+
+    public static void setupLogging() throws IOException {
+        LocalDateTime date = LocalDateTime.now();
+        FileHandler logFile = new FileHandler("logs/" + date + ".log");
+        logger.addHandler(logFile);
     }
 }
