fa92cd1cd906147ec69d345aee929ef2f3ea3e6a
SBI- <pascal.huerlimann@gmail.com>
2016-04-25 09:14:32 +0200

    Added Junit dependencies and a new file structure for tests.

---
+++ src/test/java/ch/sbi/scraper/test/mapper/BoardMapperTest.java
@@ -0,0 +1,15 @@
+package ch.sbi.scraper.test.mapper;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by sbi on 25.04.16.
+ */
+public class BoardMapperTest {
+    @Test
+    public void Test() {
+        assert true;
+    }
+
+}
