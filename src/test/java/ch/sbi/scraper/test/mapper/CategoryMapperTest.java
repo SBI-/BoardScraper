f1c088d643f39346a25e9587fec47d39c0e642d2
SBI- <pascal.huerlimann@gmail.com>
2016-04-26 11:47:11 +0200

    Added test data and a test class for CategoryMapper tests.

--- src/test/java/ch/sbi/scraper/test/mapper/CategoryMapperTest.java
+++ src/test/java/ch/sbi/scraper/test/mapper/CategoryMapperTest.java
@@ -1,7 +1,21 @@
 package ch.sbi.scraper.test.mapper;
 
+import ch.sbi.scraper.mapper.CategoryMapper;
+import ch.sbi.scraper.test.helper.TestSourceBuilder;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CategoryMapperTest {
+    private final CategoryMapper mapper;
 
+    public CategoryMapperTest() {
+        TestSourceBuilder testSourceBuilder = new TestSourceBuilder();
+        this.mapper = new CategoryMapper(testSourceBuilder);
+    }
+
+    @Test
+    public void getCategories() throws Exception {
+
+    }
 }
