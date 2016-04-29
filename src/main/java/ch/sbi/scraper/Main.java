669b06a88f74c750932923d989a9f9656c5fc993
SBI- <pascal.huerlimann@gmail.com>
2016-04-29 17:39:48 +0200

    Uncommented logging configuration until proper fix.

--- src/main/java/ch/sbi/scraper/Main.java
+++ src/main/java/ch/sbi/scraper/Main.java
@@ -5,36 +5,24 @@ import ch.sbi.scraper.example.TraverseExample;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
 
-
-    private static Logger logger = Logger.getLogger(Main.class.getName());
-
-    static {
-        try {
-//            System.setProperty("src/main/resources/java.util.logging.config.file", "default.logging.properties");
-            System.setProperty("src/main/resources/java.util.logging.config.file", "default.logging.properties");
-            LogManager manager = LogManager.getLogManager();
-            manager.readConfiguration();
-        } catch (IOException e) {
-            System.out.println("Setting up log configuration failed.");
-        }
-    }
+//    private static Logger logger = Logger.getLogger(Main.class.getName());
+//
+//    static {
+//        try {
+//            System.setProperty("java.util.logging.config.file", "default.logging.properties");
+//            LogManager manager = LogManager.getLogManager();
+//            manager.readConfiguration();
+//        } catch (IOException e) {
+//            System.out.println("Setting up log configuration failed.");
+//        }
+//    }
 
     public static void main(String[] args) {
-        try {
-            logger.info("still not working?");
-//            ThreadExample.printInfo(args[0]);
-//            ThreadExample.printPage(args[0]);
-            ThreadExample.countPages(args[0]);
-//            TraverseExample.traversePages(args[0]);
-//            ThreadExample.printThread(args[0]);
-        } catch (JAXBException e) {
-            logger.info("JAXB Error: " + e);
-            e.printStackTrace();
-        }
     }
 }
